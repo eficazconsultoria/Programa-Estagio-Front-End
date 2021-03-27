768c61143766ef75d5921b04a51c0b148ea4eaf4
Isaias <isaias.nsfilho@gmail.com>
2021-03-26 21:38:13 -0300

    finalização de estilo e funcionaliades

--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
 var form = document.querySelector(".card-form")
-form.addEventListener("click", function(event){ 
+
+form.addEventListener("submit",function(event){
     event.preventDefault()
-    console.log(event)
-    jQuery.ajax({
+    $.ajax({
         // Link da requisição que será feita para a API
         url: "https://estagio.eficazmarketing.com/api/user",
         // O tipo da requisição
         type: "POST",
         // O JSON contendo as informações necessárias para realizar a requisição, se atenda que tem requisição que não precisa enviar JSON 
         data: {
-            "seu_email": "estagiarioa@gmail.br",
-            "nome": "Estagiario",
-            "email": "vagas@eficazconsultoria.net.br",
-            "telefone": "(14) 12345-1234",
-            "rua": "Av. Higino Muzi Filho",
-            "numero": "529",
-            "complemento": "",
-            "bairro": "Mirante",
-            "cidade": "Marília",
-            "uf": "SP",
-            "cep": "17525-901"
+            "seu_email": form.elements["email-pessoal"].value,
+            "nome": form.elements["nome-c"].value,
+            "email": form.elements["email"].value,
+            "telefone": form.elements["telefone"].value,
+            "rua": form.elements["rua"].value,
+            "numero": form.elements["num"].value,
+            "complemento": form.elements["complemento"].value,
+            "bairro": form.elements["bairro"].value,
+            "cidade": form.elements["cidade"].value,
+            "uf": form.elements["uf"].value,
+            "cep": form.elements["cep"].value
         }
     }).done(function(result) {
-        // Nesta parte é o retorno da requisição, nela pode ser verificado se ocorreu tudo certo na requisição ou se ocorreu um erro.
-    });
-} )
+        alert(result.message)
+    })
+})
